7c35c8eedd659add869419f333fccfc59ab6879a
NehaVelagapudi <52523435+NehaVelagapudi@users.noreply.github.com>
2020-02-10 23:11:31 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,21 @@
+package com.epam;
+
+public class Operations {
+	public double add(double x, double y) {
+		return x + y;
+	}
+	double subtract(double x, double y) {
+		return x - y;
+	}
+	double multiply(double x, double y) {
+		return x * y;
+	}
+	double divide(double x, double y) {
+		return x / y;
+	}
+	/*public double add(double x, double y) {
+		// TODO Auto-generated method stub
+		return x + y;*/
+		//return 0;
+	
+	}
